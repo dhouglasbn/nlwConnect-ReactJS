f5e6b22d9d59a6fdbfc37d28142cf34574333eb0
dhouglas <dhouglasbn@gmail.com>
2025-02-21 17:55:33 -0300

    feature: creating aside layout

--- src/app/components/icon-button.tsx
+++ src/app/components/icon-button.tsx
@@ -1,11 +1,18 @@
 import type { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 interface IconButtonProps extends ComponentProps<'button'> {}
 
-export function IconButton(props: IconButtonProps) {
+export function IconButton({ className, ...props }: IconButtonProps) {
+  // Quando eu passo um className no IconButton e passo outra className no button aqui dentro
+  // quando className vem no {...props} ele substitui as declarações anteriores
+  // o twMerge avalia quais classes se repetem, evita redundancia e junta ambos
   return (
     <button
-      className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+      className={twMerge(
+        'p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900',
+        className
+      )}
       {...props}
     />
   )
