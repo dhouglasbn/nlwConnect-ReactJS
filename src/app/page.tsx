9f9eeecee5cb149367e8f89585516cab5705558e
dhouglas <dhouglasbn@gmail.com>
2025-02-21 14:00:41 -0300

    feature: creating inputs and buttons components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+import { ArrowRight, Copy, Mail } from 'lucide-react'
+import { Button } from "./components/button";
+import { IconButton } from "./components/icon-button";
+import { InputField, InputIcon, InputRoot } from './components/input';
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <div>
+        <Button type='submit'>
+          Enviar
+          <ArrowRight />
+        </Button>
+        <IconButton>
+          <Copy />
+        </IconButton>
+        <div>
+          <InputRoot>
+            <InputIcon>
+              <Mail />
+            </InputIcon>
+            <InputField placeholder='Digite seu email' />
+          </InputRoot>
+        </div>
+      </div>
     </main>
   );
 }
