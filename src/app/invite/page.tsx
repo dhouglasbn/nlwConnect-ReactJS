902b64187dd9631aaa7f3a74bf6faf0dfa50b5f7
dhouglas <dhouglasbn@gmail.com>
2025-02-22 11:19:12 -0300

    feature: page invite layout finished

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,6 +1,9 @@
 import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from 'lucide-react'
 import Image from 'next/image'
 import logo from '../../assets/Logo.svg'
+import cooper from '../../assets/medal_cooper.svg'
+import gold from '../../assets/medal_gold.svg'
+import silver from '../../assets/medal_silver.svg'
 import { IconButton } from '../components/icon-button'
 import { InputField, InputIcon, InputRoot } from '../components/input'
 
@@ -79,6 +82,66 @@ export default function InvitePage() {
 
               <Medal className="size-5 text-purple absolute top-3 left-3" />
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full max-w-[440px] space-y-5">
+        <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+          Ranking de indicações
+        </h2>
+
+        <div className="space-y-4">
+          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">1º</span> | Diego Fernandes
+            </span>
+
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1128
+            </span>
+
+            <Image
+              src={gold}
+              alt="gold-medal"
+              className="absolute top-0 right-8"
+            />
+          </div>
+        </div>
+
+        <div className="space-y-4">
+          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">2º</span> | Diego Fernandes
+            </span>
+
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              928
+            </span>
+
+            <Image
+              src={silver}
+              alt="silver-medal"
+              className="absolute top-0 right-8"
+            />
+          </div>
+        </div>
+
+        <div className="space-y-4">
+          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">3º</span> | Diego Fernandes
+            </span>
+
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              875
+            </span>
+
+            <Image
+              src={cooper}
+              alt="cooper-medal"
+              className="absolute top-0 right-8"
+            />
           </div>
         </div>
       </div>
