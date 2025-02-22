b92c8fc918df3eba96e87f32822874160903ae34
dhouglas <dhouglasbn@gmail.com>
2025-02-22 15:41:37 -0300

    feature: ranking works, aula 3 finished!

--- src/app/invite/[subscriberId]/ranking.tsx
+++ src/app/invite/[subscriberId]/ranking.tsx
@@ -2,8 +2,11 @@ import Image from 'next/image'
 import cooper from '../../../assets/medal_cooper.svg'
 import gold from '../../../assets/medal_gold.svg'
 import silver from '../../../assets/medal_silver.svg'
+import { getRanking } from '../../../http/api'
 
-export function Ranking() {
+export async function Ranking() {
+  const { ranking } = await getRanking()
+
   return (
     <div className="w-full max-w-[440px] space-y-5">
       <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
@@ -11,57 +14,47 @@ export function Ranking() {
       </h2>
 
       <div className="space-y-4">
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">1º</span> | Diego Fernandes
-          </span>
+        {ranking.map((item, index) => {
+          const rankingPosition = index + 1
 
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1128
-          </span>
+          return (
+            <div
+              key={item.id}
+              className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3"
+            >
+              <span className="text-sm text-gray-300 leading-none">
+                <span className="font-semibold">{rankingPosition}º</span> |{' '}
+                {item.name}
+              </span>
 
-          <Image
-            src={gold}
-            alt="gold-medal"
-            className="absolute top-0 right-8"
-          />
-        </div>
-      </div>
+              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                {item.score}
+              </span>
 
-      <div className="space-y-4">
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">2º</span> | Diego Fernandes
-          </span>
-
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            928
-          </span>
-
-          <Image
-            src={silver}
-            alt="silver-medal"
-            className="absolute top-0 right-8"
-          />
-        </div>
-      </div>
-
-      <div className="space-y-4">
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">3º</span> | Diego Fernandes
-          </span>
-
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            875
-          </span>
-
-          <Image
-            src={cooper}
-            alt="cooper-medal"
-            className="absolute top-0 right-8"
-          />
-        </div>
+              {rankingPosition === 1 && (
+                <Image
+                  src={gold}
+                  alt="gold-medal"
+                  className="absolute top-0 right-8"
+                />
+              )}
+              {rankingPosition === 2 && (
+                <Image
+                  src={silver}
+                  alt="silver-medal"
+                  className="absolute top-0 right-8"
+                />
+              )}
+              {rankingPosition === 3 && (
+                <Image
+                  src={cooper}
+                  alt="cooper-medal"
+                  className="absolute top-0 right-8"
+                />
+              )}
+            </div>
+          )
+        })}
       </div>
     </div>
   )
