d78af2630f9d041ad1bf7ec32ed5aebe98a510c1
dhouglas <dhouglasbn@gmail.com>
2025-02-22 15:32:24 -0300

    feature: finishing status details

--- src/app/invite/[subscriberId]/stats.tsx
+++ src/app/invite/[subscriberId]/stats.tsx
@@ -14,7 +14,7 @@ export async function Stats({ subscriberId }: StatsProps) {
     await getSubscribersSubscriberIdRankingClicks(subscriberId)
   const { count: inviteCount } =
     await getSubscribersSubscriberIdRankingCount(subscriberId)
-  const { position } =
+  const { position: rankingPosition } =
     await getSubscribersSubscriberIdRankingPosition(subscriberId)
 
   return (
@@ -43,7 +43,7 @@ export async function Stats({ subscriberId }: StatsProps) {
 
       <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
         <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-          {position}
+          {rankingPosition ? `${rankingPosition}º` : '-'}
         </span>
         <span className="text-sm text-gray-300 leading-none text-center">
           Posição no ranking
